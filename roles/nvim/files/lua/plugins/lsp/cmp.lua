53dcb25aa7d8168dc975262b8663015ac4875fda
Morgan Jones <me@morganlabs.dev>
2024-05-07 16:11:42 +0100

    feat: Stuff

--- roles/nvim/files/lua/plugins/lsp/cmp.lua
+++ roles/nvim/files/lua/plugins/lsp/cmp.lua
@@ -1,28 +1,28 @@
 return {
-  "hrsh7th/nvim-cmp",
-  "zbirenbaum/copilot-cmp",
-  event = "InsertEnter",
-  config = function()
-    local lsp_zero = require("lsp-zero")
-    local cmp = require("cmp")
-    local cmp_select = { behavior = cmp.SelectBehavior.Select }
-    local cmp_copilot = require("copilot_cmp")
+    "hrsh7th/nvim-cmp",
+    "zbirenbaum/copilot-cmp",
+    event = "InsertEnter",
+    config = function()
+        local lsp_zero = require("lsp-zero")
+        local cmp = require("cmp")
+        local cmp_copilot = require("copilot_cmp")
+        local cmp_select = { behavior = cmp.SelectBehavior.Select }
 
-    lsp_zero.extend_cmp()
-    cmp_copilot.setup()
+        lsp_zero.extend_cmp()
+        cmp_copilot.setup()
 
-    cmp.setup({
-      formatting = lsp_zero.cmp_format(),
-      sources = {
-        { name = "copilot" },
-        { name = "nvim_lsp" },
-      },
-      mapping = cmp.mapping.preset.insert({
-        ["<C-j>"] = cmp.mapping.select_next_item(cmp_select),
-        ["<C-k>"] = cmp.mapping.select_prev_item(cmp_select),
-        ["<S-Tab>"] = cmp.mapping.confirm({ select = true }),
-        ["<C-Space>"] = cmp.mapping.complete(),
-      }),
-    })
-  end,
+        cmp.setup({
+            formatting = lsp_zero.cmp_format(),
+            sources = {
+                { name = "copilot" },
+                { name = "nvim_lsp" },
+            },
+            mapping = cmp.mapping.preset.insert({
+                ["<C-j>"] = cmp.mapping.select_next_item(cmp_select),
+                ["<C-k>"] = cmp.mapping.select_prev_item(cmp_select),
+                ["<S-Tab>"] = cmp.mapping.confirm({ select = true }),
+                ["<C-Space>"] = cmp.mapping.complete(),
+            }),
+        })
+    end,
 }
